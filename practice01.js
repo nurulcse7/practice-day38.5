31b890fb129a03afde962b5431f5e6b67e86b7ef
nurulcse7 <nurul.cse7@gmail.com>
2022-09-13 19:20:24 +0600

    Updated practice 01 and 02

--- practice01.js
+++ practice01.js
@@ -1,11 +1,30 @@
 /*
-
+Practice task for local storage ("Level -1")
+01. Clear your local storage;
+02. Set a local storage item. key: name, value: Shakib Khan;
+03. Set a local storage item. key: age, value: 40;
+04. Get the local storage item you have set.
+05. Remove the local storage key 'name' & key 'age'
+06. Set a object as local storage item. The object is {firstName: 'Abraham', lastName: 'Linkon'}
 */
 
+// 01.
+localStorage.clear()
 
+// 02. 
+localStorage.setItem('name', 'Nurul Islam')
 
-// node practice01.js
+// 03. 
+localStorage.setItem('age', 30)
 
-/*
+// 04. 
+localStorage.getItem('name') // 'Nurul Islam'
+localStorage.getItem('age')  // '32'
 
-*/
+// 05. 
+localStorage.removeItem('name')
+localStorage.removeItem('age')
+
+// 06. 
+
+// need help 06
--- practice02.js
+++ practice02.js
@@ -1,11 +1,27 @@
 /*
-
+Practice task for local storage ("Level -2") 
+Step-1: HTML file will have a p tag and a button.
+Step-2: The p tag will show "the count" which is initially 0. 
+        (initially the value in local storage is 0)
+Step-3: Every time, after clicking the button the number will be increased by 1. 
+        and the result will be show in the p tag.
+Step-4: With every click on the button, the result will be stored in the local storage also.
+Step-5: Now the result is stored in local storage. So, if you reload the page or close the tab and open again,
+        the result of "the count" is not 0. Rather it will show the value from local storage without reset.
 */
+// ref. 28-1
+// console.log('first')
 
+document.getElementById('increase-value').addEventListener('click', function(){
+    // console.log('button clicked')
+    const increaseField = document.getElementById('increase-field')
+    const increaseValueString = increaseField.innerText;
+    const previousIncreaseValue = parseInt(increaseValueString)
+    // console.log(previousIncreaseValue);
+    const newIncreaseNumber = previousIncreaseValue + 1;
+    increaseField.innerText = newIncreaseNumber;
+    console.log(newIncreaseNumber)
+    localStorage.setItem()
+})
 
-
-// node practice02.js
-
-/*
-
-*/
+// need help 4 and 5
